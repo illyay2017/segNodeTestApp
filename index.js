258201c85463452a64c987027fcff672006f156d
Illya Yanchuk <illya.yanchuk@illya.yanchuk>
2018-09-18 12:41:13 -0700

    finished setting up basic skeleton and env variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const segmentWriteKey = process.env.SEG_WRITE_KEY;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
